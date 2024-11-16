287538fec67ac4d6a5d9d43392e85c5c3d111ef6
MuhammadTalha15 <talhaz5518@gmail.com>
2024-11-16 21:56:29 +0500

    LAyout Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import './App.css'
-import pic from './assets/profile-pic.png'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +9,13 @@ function App() {
       <main className='mn'>
         <section className='vw-prt'>
           <div className="profile-pic-cnt">
-            <div className='profile-pic'></div>
+            <div className="profile-pic"></div>
           </div>
-            <h2 className="name">Muhammad Talha</h2>
-            <p className='des'>Programmer | Web Designer | Student</p>
+          <h1 className="name">MUHAMMAD TALHA</h1>
+          <h3 className="des">Web Developer | Programmer | Student</h3>
+          <h2 className="cont-head">Biography</h2>
+          <p className="txt-des">Hey! I'm Muhammad Talha - a Web designer, developer and Programmer breathing in Islamabad, Pakistan</p>
+          <h2 className="cont-head">Social Links</h2>
           <div className="scl-cnt">
             <a title='Facebook' href="https://www.facebook.com/profile.php?id=100092518021987" className='scl f'></a>
             <a title='Instagram' href="https://www.instagram.com/ttlha._15" className='scl i'></a>
@@ -21,7 +23,7 @@ function App() {
             <a title='Email' href="https://mail.google.com" className='scl m'></a>
             <a title='Pinterest' href="https://www.pinterest.com/talhaz5518" className='scl p'></a>
             <a title='Stack-Overflow' href="#" className='scl s'></a>
-            <a title='Website' href="https://muhammadtalha-website.vercel.app" className='scl w'></a>
+            <a title='Website' href="https://mtalha-website.vercel.app" className='scl w'></a>
           </div>
         </section>
       </main>
