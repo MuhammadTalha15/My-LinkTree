9069a45357008cede56f63b645bb11a9f252231a
MuhammadTalha15 <talhaz5518@gmail.com>
2025-01-26 17:09:11 +0500

    Layout Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './styles/App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,20 +12,34 @@ function App() {
           <div className="profile-pic-cnt">
             <div className="profile-pic"></div>
           </div>
-          <h1 className="name">MUHAMMAD TALHA</h1>
-          <h3 className="des">Web Developer | Programmer | Student</h3>
-          <h2 className="cont-head">Biography</h2>
-          <p className="txt-des">Hey! I'm Muhammad Talha - a Web designer, developer and Programmer breathing in Islamabad, Pakistan</p>
+          <h1 className="name">Muhammad Talha</h1>
+          <h3 className="des">Programmer | Tech Enthusiast</h3>
           <h2 className="cont-head">Social Links</h2>
+
           <div className="scl-cnt">
-            <a title='Facebook' href="https://www.facebook.com/profile.php?id=100092518021987" className='scl f'></a>
-            <a title='Instagram' href="https://www.instagram.com/ttlha._15" className='scl i'></a>
-            <a title='Github' href="https://www.github.com/MuhammadTalha15" className='scl g'></a>
-            <a title='Email' href="https://mail.google.com" className='scl m'></a>
-            <a title='Pinterest' href="https://www.pinterest.com/talhaz5518" className='scl p'></a>
-            <a title='Stack-Overflow' href="#" className='scl s'></a>
-            <a title='Website' href="https://mtalha-website.vercel.app" className='scl w'></a>
+
+            <a title='Facebook' href="https://www.facebook.com/profile.php?id=100092518021987" className='scl-btn' target='_blank'>
+             <div className='scl f'></div>
+             <span className='face-tag'>Facebook</span>
+            </a>
+
+            <a title='Facebook' href="https://www.instagram.com/ttlha._15" className='scl-btn' target='_blank'>
+             <div className='scl i'></div>
+             <span className='face-tag'>Instagram</span>
+            </a>
+
+            <a title='Facebook' href="https://www.github.com/MuhammadTalha15" className='scl-btn' target='_blank'>
+             <div className='scl g'></div>
+             <span className='face-tag'>Github</span>
+            </a>
+
+            <a title='Facebook' href="https://muhammadtalha-portfolio.vercel.app" className='scl-btn' target='_blank'>
+             <div className='scl w'></div>
+             <span className='face-tag'>Website</span>
+            </a>
+
           </div>
+          <p className="ftr">Designed by | Muhammad Talha</p>
         </section>
       </main>
     </>
